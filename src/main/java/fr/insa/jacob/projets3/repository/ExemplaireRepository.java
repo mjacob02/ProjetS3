316366208a17577534f7d46eda0e0aa370947b13
laptop <ghada.jarboui@insa-strasbourg>
2023-12-28 16:19:00 +0100

    test ghada

--- src/main/java/fr/insa/jacob/projets3/repository/ExemplaireRepository.java
+++ src/main/java/fr/insa/jacob/projets3/repository/ExemplaireRepository.java
@@ -4,6 +4,9 @@ import fr.insa.jacob.projets3.entity.Exemplaire;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface ExemplaireRepository extends JpaRepository<Exemplaire, Integer>, JpaSpecificationExecutor<Exemplaire> {
 
+    List<Exemplaire> search(String stringFilter);
 }
