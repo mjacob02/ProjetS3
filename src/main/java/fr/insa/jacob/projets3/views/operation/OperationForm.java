20356ad12bca9674bdbac8841fc29b65bfeed7d5
vimbert01 <valentvavava@gmail.com>
2024-01-05 20:51:11 +0100

    corrections operationForm

--- src/main/java/fr/insa/jacob/projets3/views/operation/OperationForm.java
+++ src/main/java/fr/insa/jacob/projets3/views/operation/OperationForm.java
@@ -15,6 +15,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.shared.Registration;
+import fr.insa.jacob.projets3.entity.TypeOperation;
 
 import java.util.List;
 
@@ -30,21 +31,12 @@ public class OperationForm extends FormLayout {
     Button save = new Button("Save");
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
+
     // Other fields omitted
-    /*Binder<Produit> binder = new BeanValidationBinder<>(Produit.class); // To validate the form
+    Binder<Operation> binder = new BeanValidationBinder<>(Operation.class); // To validate the form
 
-    public ProduitForm(List<Gamme> gammes) {
-        addClassName("produit-form"); // To style the form with CSS
-        binder.bindInstanceFields(this);  // To bind the fields of the form to the fields of the entity
-
-        gamme.setItems(gammes);
-        gamme.setItemLabelGenerator(Gamme::getReference);  // To display the name of the company in the combo box
-
-        add(reference,
-                description,
-                gamme,
-                createButtonsLayout()); // To display the fields in the form
-    }
+    public OperationForm(List<TypeOperation> typeOperation) {
+        }
 
     private Component createButtonsLayout() {
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
@@ -55,8 +47,8 @@ public class OperationForm extends FormLayout {
         close.addClickShortcut(Key.ESCAPE);
 
         save.addClickListener(event -> validateAndSave());
-        delete.addClickListener(event -> fireEvent(new fr.insa.jacob.projets3.views.produit.ProduitForm.DeleteEvent(this, binder.getBean())));
-        close.addClickListener(event -> fireEvent(new fr.insa.jacob.projets3.views.produit.ProduitForm.CloseEvent(this)));
+        delete.addClickListener(event -> fireEvent(new OperationForm.DeleteEvent(this, binder.getBean())));
+        close.addClickListener(event -> fireEvent(new OperationForm.CloseEvent(this)));
 
         binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
         return new HorizontalLayout(save, delete, close);
@@ -64,20 +56,20 @@ public class OperationForm extends FormLayout {
 
     private void validateAndSave() {
         if (binder.isValid()) {
-            fireEvent(new fr.insa.jacob.projets3.views.produit.ProduitForm.SaveEvent(this, binder.getBean()));
+            fireEvent(new OperationForm.SaveEvent(this, binder.getBean()));
         }
     }
 
 
-    public void setProduit(Produit produit) {
-        binder.setBean(produit);    // To fill the form with the data of the selected entity
+    public void setOperation(Operation operation) {
+        binder.setBean(operation);    // To fill the form with the data of the selected entity
     }
-*/
+
     // Events
-    public static abstract class OperationFormEvent extends ComponentEvent<fr.insa.jacob.projets3.views.operation.OperationForm> {
+    public static abstract class OperationFormEvent extends ComponentEvent<OperationForm> {
         private Operation operation;
 
-        protected OperationFormEvent(fr.insa.jacob.projets3.views.operation.OperationForm source, Operation operation) {
+        protected OperationFormEvent(OperationForm source, Operation operation) {
             super(source, false);
             this.operation = operation;
         }
@@ -87,35 +79,35 @@ public class OperationForm extends FormLayout {
         }
     }
 
-    public static class SaveEvent extends fr.insa.jacob.projets3.views.operation.OperationForm.OperationFormEvent {
-        SaveEvent(fr.insa.jacob.projets3.views.operation.OperationForm source, Operation operation) {
+    public static class SaveEvent extends OperationForm.OperationFormEvent {
+        SaveEvent(OperationForm source, Operation operation) {
             super(source, operation);
         }
     }
 
-    public static class DeleteEvent extends fr.insa.jacob.projets3.views.operation.OperationForm.OperationFormEvent {
-        DeleteEvent(fr.insa.jacob.projets3.views.operation.OperationForm source, Operation operation) {
+    public static class DeleteEvent extends OperationFormEvent {
+        DeleteEvent(OperationForm source, Operation operation) {
             super(source, operation);
         }
 
     }
 
-    public static class CloseEvent extends fr.insa.jacob.projets3.views.operation.OperationForm.OperationFormEvent {
-        CloseEvent(fr.insa.jacob.projets3.views.operation.OperationForm source) {
+    public static class CloseEvent extends OperationFormEvent {
+        CloseEvent(OperationForm source) {
             super(source, null);
         }
     }
 
-    public Registration addDeleteListener(ComponentEventListener<fr.insa.jacob.projets3.views.operation.OperationForm.DeleteEvent> listener) {
-        return addListener(fr.insa.jacob.projets3.views.operation.OperationForm.DeleteEvent.class, listener);
+    public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+        return addListener(DeleteEvent.class, listener);
     }
 
-    public Registration addSaveListener(ComponentEventListener<fr.insa.jacob.projets3.views.operation.OperationForm.SaveEvent> listener) {
-        return addListener(fr.insa.jacob.projets3.views.operation.OperationForm.SaveEvent.class, listener);
+    public Registration addSaveListener(ComponentEventListener<SaveEvent> listener) {
+        return addListener(SaveEvent.class, listener);
     }
 
-    public Registration addCloseListener(ComponentEventListener<fr.insa.jacob.projets3.views.operation.OperationForm.CloseEvent> listener) {
-        return addListener(fr.insa.jacob.projets3.views.operation.OperationForm.CloseEvent.class, listener);
+    public Registration addCloseListener(ComponentEventListener<CloseEvent> listener) {
+        return addListener(CloseEvent.class, listener);
     }
 }
 
